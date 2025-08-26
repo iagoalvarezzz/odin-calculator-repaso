f8d5a9c91d6d669e0f8f9e4e6dce5d6edb0842ed
iagoalvarezzz <alvarezsolacheiago@gmail.com>
2025-08-26 14:21:50 +0200

    Add basic operate functions

---
+++ script.js
@@ -0,0 +1,33 @@
+function add (a,b){
+  return a + b;
+}
+
+function substract(a,b){
+  return a - b;
+}
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
+function operate(a,b,operator){
+  switch(operator){
+    case "+":
+      return add(a,b);
+    case "-":
+      return substract(a,b);
+    case "*":
+      return multiply(a,b);
+    case "/":
+      return divide(a,b);
+  }
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "+";
+
