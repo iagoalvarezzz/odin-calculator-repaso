85a1c9c941715b39eb406ea629ce5c93a3799cdb
iagoalvarezzz <alvarezsolacheiago@gmail.com>
2025-08-26 15:06:15 +0200

    Code full js

--- script.js
+++ script.js
@@ -27,7 +27,69 @@ function operate(a,b,operator){
   }
 }
 
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "+";
+function assignNumber(button){
+  if (operator !== ""){
+    secondNumber += button.getAttribute("id");
+  } else if (result){
+    clear();
+    firstNumber += button.getAttribute("id");
+    result = false;
+  } else{
+    firstNumber += button.getAttribute("id");
+  }
+}
 
+function assignOperator(button){
+  if (firstNumber != "" && secondNumber === ""){
+    operator = button.getAttribute("id");
+  } else if (firstNumber !== "" && secondNumber !== ""){
+    execute();
+    operator = button.getAttribute("id");
+  }
+}
+
+function execute(){
+  if (firstNumber === "" || secondNumber === "" || operator === "") return;
+  if (+secondNumber === 0 && operator === "/"){
+    clear();
+    return;
+  }
+
+  firstNumber = Math.floor(operate(+firstNumber,+secondNumber,operator));
+  operator = "";
+  secondNumber = "";
+  result = true;
+}
+
+function clear(){
+  firstNumber = "";
+  operator = "";
+  secondNumber = "";
+}
+
+function buttonClicked(event){
+  const button = event.target;
+  if (button.classList.contains("buttons")) return;
+  if (button.parentElement.classList.contains("numbers")){
+    assignNumber(button);
+  } else if (button.parentElement.classList.contains("operators")){
+    assignOperator(button);
+  } else if (button.getAttribute("id") === "exe"){
+    execute();
+  } else if (button.getAttribute("id") === "clear"){
+    clear(button);
+  }
+
+  display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+
+}
+
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let result = false;
+
+const buttonsContainer = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+
+buttonsContainer.addEventListener("click", buttonClicked);
